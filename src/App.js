bf628a1b2229e5eecacc390147267bad451a3183
TatianaChorba <moliantv@gmail.com>
2020-10-14 14:45:04 +0300

    Init

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import Header from './components/header/header';
+import Main from './components/main/main';
+import Feature from './components/feature/feature';
+import Footer from './components/footer/footer';
+import Calendar from './components/calendar/calendar';
+import Detail from './components/detail/detail';
+import './style.css';
+
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Feature />
+      <Footer />
+      <Calendar />
+      {/* <Detail /> */}
+    </>
+  );
+}
+
+export default App;
